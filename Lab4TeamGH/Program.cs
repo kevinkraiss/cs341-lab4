b02ff0d9db279d323a27d9c98fe6cab8097a0747
Kevin Kraiss <kevinkraiss@gmail.com>
2025-10-05 15:46:46 -0500

    Implement Calculator class as specified in Step 3.2

--- Lab4TeamGH/Program.cs
+++ Lab4TeamGH/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(Calculator.Add(3, 4));
+        Console.WriteLine(Calculator.Subtract(3, 4));
+    }
+}
+public static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return 0;
+    }
+    public static int Subtract(int a, int b)
+    {
+        return 0;
+    }
+    public static int Multiply(int a, int b)
+    {
+        return 0;
+    }
+    public static int Divide(int a, int b)
+    {
+        return 0;
+    }
+}
